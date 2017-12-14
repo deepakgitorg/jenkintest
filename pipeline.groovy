1de30b4ca24803cb159cea7a2cb69985322fd348
Deepak Jena <deepak.jena@mindtree.com>
2017-12-14 14:24:53 -0500

    jenkins file

---
+++ pipeline.groovy
@@ -0,0 +1,31 @@
+pipeline{
+	agent any
+	stages{
+		stage('checkout'){
+			steps{
+				println 'check out from GIT !!!'
+			}
+		}
+		stage('Code Analysis'){
+			steps{
+				echo 'Code Analysis'
+			}
+		}
+		stage('Build'){
+			steps{
+				echo 'Build'
+			}
+		}
+		stage('Test Automation'){
+			steps{
+				echo 'Test Automation'
+			}
+		}
+		stage('Test Automation'){
+			steps{
+				echo 'Test Automation'
+			}
+		}
+	
+	}
+}
